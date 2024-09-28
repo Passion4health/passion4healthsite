20d9fd46ad6fc31e1f294ce1849852dde2861b56
sammy-techzard <nisammy40@gmail.com>
2024-09-28 18:13:56 +0200

    Switched to prod without adding static files serving

--- passion4health/urls.py
+++ passion4health/urls.py
@@ -1,6 +1,8 @@
 from django.conf import settings
 from django.urls import include, path
 from django.contrib import admin
+from django.urls import re_path
+from django.views.static import serve
 
 from wagtail.admin import urls as wagtailadmin_urls
 from wagtail import urls as wagtail_urls
