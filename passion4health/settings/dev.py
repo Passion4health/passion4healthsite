c8c6fc1a6ac2bc223299707e4164f02a6ddb9405
sammy-techzard <nisammy40@gmail.com>
2024-09-28 18:43:56 +0200

    changed the 404 error page

--- passion4health/settings/dev.py
+++ passion4health/settings/dev.py
@@ -1,7 +1,7 @@
 from .base import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "django-insecure-j&!)*jv%m61)_zzx*k9%c(q32res)m_*&@#*ox779%kn8n^7j7"
